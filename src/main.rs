9e397560d7dd0bccc7e9131339315d023079b89c
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-09 22:46:55 -0500

    scan func added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::env;
-use std::net::IpAddr;
+use std::{env, num};
+use std::io::{self,Write}
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
 use std::sync::mpsc::{Sender,channel};
 use std::thread;
 
+const MAX: u16=65535;
 struct  Arguments{
     flag: String,
     ipaddr:IpAddr,
@@ -47,6 +49,23 @@ return Ok(Arguments { flag, ipaddr, threads});
     }
 }
 
+fn scan(tx::Sender<u16>,start_port:u16,addr:IpAddr,num_threads:u16){
+    let mut port:u16= start_port +1;let mut port:u16=start_port
+    loop{
+        match  TcpStream::connect((addr,port)) {
+            Ok(_)=>{
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_)=>{}
+        }
+        if (MAX -port)< num_threads{
+            break;
+        }
+        port+=num_threads;
+    }
+}
 fn main() {
     let args: Vec<String>=env::args().collect();
     let program = args[0].clone();
@@ -62,6 +81,12 @@ fn main() {
         }
     );
     let num_threads= arguments.threads;
-    let (tx.rx)= channel();
-    
+    let (tx,rx)= channel();
+    for i in 0 num_threads{
+        let tx =tx.clone();
+        thread::spawn(move||{
+            sc an(tx,i,arguments.ipaddr,num_threads);
+        });
+    }
+
 }
